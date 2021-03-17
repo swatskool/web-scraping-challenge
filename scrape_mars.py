8256488492b5b45115d17858a99afc273b61ba37
swatskool <75335665+swatskool@users.noreply.github.com>
2021-03-17 07:48:56 -0600

    again

--- scrape_mars.py
+++ scrape_mars.py
@@ -3,8 +3,7 @@ from bs4 import BeautifulSoup
 
 
 def init_browser():
-    # @NOTE: Replace the path with your actual path to the chromedriver
-    executable_path = {"executable_path": "/usr/local/bin/chromedriver"}
+    executable_path = {"executable_path": ChromeDriverManager().install()}
     return Browser("chrome", **executable_path, headless=False)
 
 
